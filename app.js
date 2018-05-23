ab131a8b30e0c1031a437faafe2e545d266f8faa
Damián Adams <contact@mianlabs.com>
2018-05-22 23:27:45 -0400

    Configure some general express middleware and express-session

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 require('dotenv').config();
 
-const express = require('express'),
+const path = require('path'),
+	express = require('express'),
 	app = express(),
 	session = require('express-session'),
+	flash = require('connect-flash'),
 	passport = require('passport'),
 	SpotifyStrategy = require('passport-spotify').Strategy;
 
+// GENERAL EXPRESS SETUP
 app.set('view engine', 'ejs');
+const publicPath = path.resolve(__dirname, 'public');
+app.use(express.static(publicPath));
+app.use(express.urlencoded({extended: true})); // bodyparser
+
+// SESSION SETUP
+app.use(session({
+	secret: process.env.SESSION_SECRET || 'Debug Session Key',
+	resave: false,
+	saveUninitialized: false,
+	cookie: {
+		maxAge: 1000 * 60 * 60 * 24 * 7 * 2 // 2 weeks
+	}
+}));
+app.use(flash());
 
 app.get('/', (req, res) => {
 	res.send('Hello world!');
