6e36ac7e18e8133ed6310edbebb18043152ce9a4
Damián Adams <contact@mianlabs.com>
2018-06-07 19:54:13 -0400

    Add semantic-ui npm module, build it, and create the index view

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-	return res.send('Welcome to Tastyfi.me');
+	return res.render('index');
 });
 
 module.exports = router;
