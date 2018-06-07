e8c101eb5af9ccaf1196efd0de199cd6a26217af
Damián Adams <contact@mianlabs.com>
2018-06-06 20:56:44 -0400

    Add the provisional audio route

--- routes/users.js
+++ routes/users.js
@@ -14,7 +14,7 @@ router.get('/', refreshAuth, async (req, res, next) => {
 			throw new Error('User not found.');
 		}
 		// Request data from Spotify concurrently
-		const data = await Promise.all([
+		const responses = await Promise.all([
 			SpotifyService.getUserProfile(user.spotifyId, user.access.accessToken),
 			SpotifyService.getTopArtists(user.access.accessToken, {
 				limit: '50',
@@ -26,16 +26,40 @@ router.get('/', refreshAuth, async (req, res, next) => {
 			})
 		]);
 		// Check responses
-		data.forEach(response => {
+		responses.forEach(response => {
 			if (response.statusCode !== 200) {
 				throw new Error(JSON.stringify(response.body));
 			}
 		});
 		return res.json({
-			profile: data[0].body,
-			topArtists: data[1].body,
-			topTracks: data[2].body
+			profile: responses[0].body,
+			topArtists: responses[1].body,
+			topTracks: responses[2].body
 		});
+	} catch (err) {
+		return next(err);
+	}
+});
+
+router.get('/audio', refreshAuth, async (req, res, next) => {
+	try {
+		const user = await User.findById(req.params.id);
+		if (!user) {
+			throw new Error('User not found');
+		}
+		const trackResponse = await SpotifyService.getTopTracks(user.access.accessToken, {
+			limit: '50',
+			time_range: 'long_term'
+		});
+		if (trackResponse.statusCode !== 200) {
+			throw new Error(JSON.stringify(trackResponse.body));
+		}
+		const topTracks = trackResponse.body.items.map(i => i.id);
+		const audioResponse = await SpotifyService.getAudioFeatures(user.access.accessToken, topTracks);
+		if (audioResponse.statusCode !== 200) {
+			throw new Error(JSON.stringify(audioResponse.body));
+		}
+		return res.json(audioResponse.body);
 	} catch (err) {
 		return next(err);
 	}
