ff15954289ef16bdc35837335ac23f704acac45e
Damián Adams <contact@mianlabs.com>
2018-06-14 20:19:52 -0400

    Process the data from the audio route

--- routes/users.js
+++ routes/users.js
@@ -60,7 +60,44 @@ router.get('/audio', refreshAuth, async (req, res, next) => {
 		if (audioResponse.statusCode !== 200) {
 			throw new Error(JSON.stringify(audioResponse.body));
 		}
-		return res.json(audioResponse.body);
+
+		const analysis = audioResponse.body;
+		const count = analysis.audio_features.length;
+
+		const featureTallies = {
+			keys: new Array(12).fill(0), // Keys are in Pitch Class notation (0-11)
+			modes: new Array(2).fill(0) // 0 - Minor, 1 - Major
+		};
+		const featureSums = analysis.audio_features.reduce((acc, curr) => {
+			// Update tallies
+			featureTallies.keys[curr.key] += 1;
+			featureTallies.modes[curr.mode] += 1;
+
+			// Update sums
+			Object.keys(acc).forEach(feature => {
+				acc[feature] += curr[feature];
+			});
+			return acc;
+		}, {
+			acousticness: 0,
+			danceability: 0,
+			duration_ms: 0,
+			energy: 0,
+			instrumentalness: 0,
+			liveness: 0,
+			tempo: 0,
+			valence: 0
+		});
+
+		const featureAverages = {};
+		Object.keys(featureSums).forEach((feature) => {
+			featureAverages[feature] = featureSums[feature] / count;
+		});
+
+		return res.json({
+			feature_tallies : featureTallies,
+			feature_averages: featureAverages
+		});
 	} catch (err) {
 		return next(err);
 	}
